fedf86af7686ab390412a1d9c94bb4788ded03ab
weldson01 <weldsoncarlos007@gmail.com>
2024-06-11 20:42:06 -0300

    feat: search input

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,12 @@
 import { Header } from "../../layout/header/Header"
+import { Search } from "./components/Search"
 
 
-export const Home = ()=>{
-    return <><Header/></>
+export const Home = () => {
+    return <>
+        <Header />
+        <main className="flex flex-col">
+            <Search />
+        </main>
+    </>
 }
