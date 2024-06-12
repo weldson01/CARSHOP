36ef0048a2aef1cf71ad414abcc8f85922b9b457
weldson01 <weldsoncarlos007@gmail.com>
2024-06-12 00:15:40 -0300

    cleaning

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, RouterProvider, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "../pages";
 
 
